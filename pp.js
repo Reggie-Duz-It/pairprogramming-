bf42fb5e0ed5da7c5b37cad9c11055482339a92b
JorgeLlanes <102987755+JorgeLlanes@users.noreply.github.com>
2022-05-02 13:27:23 -0500

    Delete pp.js
    delete

--- pp.js
+++
@@ -1,6 +0,0 @@
-//Easy (1)
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-let odds = arr.filter( n => n % 2);
-let evens = arr.filter( n => n % 2 == 0);
-console.log(odds);
-console.log(evens);
